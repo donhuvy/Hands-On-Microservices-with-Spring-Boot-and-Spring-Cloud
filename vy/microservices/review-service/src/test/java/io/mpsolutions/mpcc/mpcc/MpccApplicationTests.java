be376f7be2432ac444419e403d111fadad6f085e
Do Nhu Vy <donhuvy@outlook.com>
2020-05-10 23:35:02 +0700

    update

---
+++ vy/microservices/review-service/src/test/java/io/mpsolutions/mpcc/mpcc/MpccApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mpsolutions.mpcc.mpcc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
